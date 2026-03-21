beb0d0a9f004fe440668eb0e8f065cc52365abe6
ravendr17 <ravendr17@gmail.com>
2026-03-21 20:55:15 +0800

    fixed-ticket-seeder
    Fixed ticket seeder so now upon running migrate:fresh --seed the tickets table will have 3 'test' tickets for 1 license, to demonstrate the total_fine calculation according to the ticket service and the violations_lookup table

--- database/seeders/TicketSeeder.php
+++ database/seeders/TicketSeeder.php
@@ -2,46 +2,39 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Services\TicketService;
+use App\DTOs\CreateTicketRequest;
 use Illuminate\Support\Facades\DB;
+use DateTime;
+
 class TicketSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
-    {   
-        $ticket_fine = DB::table('ticket_items')->insertGetId([
-            'ticket_id' => 1,
-            'violation_id' => 7,
-            'name' => 'Driving Without Valid License',
-            'fine' => 1000,
-        ]);
-        DB::table('tickets')->insert([
-            'ticket_id' => 1,
-            'license_id' => 1,
-            'lto_client_id' => '123456789',
-            'ref_number' => '1234567890',
-            'date_of_incident' => now(),
-            'place_of_incident' => 'Manila',
-            'notes' => 'test',
-            'status' => 'Unsettled',
-            'total_fine' => $ticket_fine,
-            'created_at' => now(),
-            'updated_at' => now(),
-            // 'ticket_id' => 1,
-            // 'license_id' => 1,
-            // 'lto_client_id' => '123456789',
-            // 'ref_number' => '1234567890',
-            // 'violations' => 1,
-            // 'date_of_incident' => now(),
-            // 'place_of_incident' => 'Manila',
-            // 'notes' => 'test',
-            // 'status' => 'Unsettled',
-            // 'total_fine' => 1000,
-            // 'created_at' => now(),
-            // 'updated_at' => now(),
-        ]);
+    {
+        $license = DB::table('licenses')->first();
+        $service = app(TicketService::class);
+
+        // Create 3 tickets for the same license to test the total_fine
+        for ($i = 1; $i <= 3; $i++) {
+            try {
+                $request = new CreateTicketRequest(
+                    licenseNumber: $license->license_number,
+                    violationIds: [1, 10],
+                    dateOfIncident: new DateTime(),
+                    placeOfIncident: 'City of Manila',
+                    notes: "Test Ticket #{$i} for total_fine calculation",
+                    proofImage: "proof_{$i}.jpg"
+                );
+
+                $ticketId = $service->createTicket($request);
+
+            } catch (\Exception $e) {
+                $this->command->error("Failed at Ticket #{$i}: " . $e->getMessage());
+            }
+        }
     }
 }
